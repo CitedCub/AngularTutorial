5e15dfea19a2457d589392aa4248375627a1cd90
Andreas Erlandsson <andreas.erlandsson@gmail.com>
2017-04-29 23:53:47 +0200

    Working with routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
@@ -10,10 +12,30 @@ import { HeroService }          from './hero.service';
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent, 
+    DashboardComponent,
     HeroDetailComponent, 
     HeroesComponent 
   ],
@@ -24,4 +46,5 @@ import { HeroService }          from './hero.service';
     AppComponent 
   ]
 })
-export class AppModule { }
+export class AppModule {
+}
