3788e64230d9e4e1364b63ad8e38d5bdb85edd0a
Andreas Erlandsson <andreas.erlandsson@gmail.com>
2017-04-30 08:48:55 +0200

    Before 'Add the ability to add heroes'

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -9,7 +9,7 @@ import { HeroService } from './hero.service';
 @Component({
     selector: 'hero-detail',
     templateUrl: `./hero-detail.component.html`,
-    styleUrls: [ './hero-detail.component.css' ]
+    styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
     hero: Hero;
@@ -28,5 +28,10 @@ export class HeroDetailComponent implements OnInit {
 
     goBack(): void {
         this.location.back();
+    }
+
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
     }
 }
